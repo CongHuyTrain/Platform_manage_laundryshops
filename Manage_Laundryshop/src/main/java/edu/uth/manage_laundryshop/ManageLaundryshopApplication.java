e96464e524b15b9a1fe4445a2ccc663494984178
CongHuyTrain <leconghuy2005x1@gmail.com>
2025-03-29 22:21:44 +0700

    base

---
+++ Manage_Laundryshop/src/main/java/edu/uth/manage_laundryshop/ManageLaundryshopApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.manage_laundryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageLaundryshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageLaundryshopApplication.class, args);
+    }
+
+}
---
+++ Manage_Laundryshop/src/test/java/edu/uth/manage_laundryshop/ManageLaundryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.manage_laundryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageLaundryshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
