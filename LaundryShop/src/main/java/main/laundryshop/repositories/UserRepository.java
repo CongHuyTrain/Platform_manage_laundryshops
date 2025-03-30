924dba61c532b5262a041a68b7bda973ff252e43
CongHuyTrain <leconghuy2005x1@gmail.com>
2025-03-30 17:37:53 +0700

    sửa user reponsitory

--- LaundryShop/src/main/java/main/laundryshop/repositories/UserRepository.java
+++ LaundryShop/src/main/java/main/laundryshop/repositories/UserRepository.java
@@ -1,13 +1,27 @@
 package main.laundryshop.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import main.laundryshop.models.User;
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    boolean existsByName(String Name);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    default boolean existsByUsername(String username) {
+        return false;
+    }
+    default User save(User user) {
+        return null;
+    }
 
-    Optional<User> findByName(String Name);
+    default List<User> findAll() {
+        return null;
+    }
+    default Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
 }
+
