775f00800eea9992ade1cbe48c848aad451bb11e
YenVy168 <vyty0511@ut.edu.vn>
2025-04-12 23:53:43 +0700

    Dangki

--- LaundryShop/src/main/java/main/laundryshop/service/CustomerService.java
+++ LaundryShop/src/main/java/main/laundryshop/service/CustomerService.java
@@ -1,40 +1,17 @@
 package main.laundryshop.service;
 
-import main.laundryshop.models.Customer;
-import main.laundryshop.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
-
 @Service
 public class CustomerService {
+
     @Autowired
-    private CustomerRepository customerRepository;
+    CustomerRespository customerRepository;
 
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
-    }
-
-    public Customer createCustomer(Customer customer) {
+    public Customer createCustomer (Customer customer) {
         return customerRepository.save(customer);
-    }
 
-    public Customer updateCustomer(UUID id, Customer customer) {
-        Customer existingCustomer = customerRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Customer not found"));
-        existingCustomer.setName(customer.getName());
-        existingCustomer.setPhone(customer.getPhone());
-        existingCustomer.setEmail(customer.getEmail());
-        existingCustomer.setAddress(customer.getAddress());
-        existingCustomer.setOrderCount(customer.getOrderCount());
-        existingCustomer.setType(customer.getType());
-        existingCustomer.setNote(customer.getNote());
-        return customerRepository.save(existingCustomer);
-    }
-
-    public void deleteCustomer(UUID id) {
-        customerRepository.deleteById(id);
     }
 }
+
